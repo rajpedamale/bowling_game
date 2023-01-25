fe7ab314c5523fd0d329efffea56af838198b220
ZT6Guy <ZT6Guy@cyber-dojo.org>
2023-01-25 06:25:50 +0000

    15 ran tests, predicted green, got green

--- files/bowling_game.js
+++ files/bowling_game.js
@@ -1,12 +1,15 @@
 'use strict';
 
 function bowling_game() {
+  let game_score = 0;
+  
   const roll = pins => {
+    game_score += pins;
     return pins;
   };
   
   const score = () => {
-    return 0;
+    return game_score;
   };
   
   return {
