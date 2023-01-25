cb80381d385a4c07d3dfe581e3483ed495db6c01
ZT6Guy <ZT6Guy@cyber-dojo.org>
2023-01-25 06:32:11 +0000

    16 ran tests, predicted green, got green

--- files/bowling_game.test.js
+++ files/bowling_game.test.js
@@ -1,30 +1,42 @@
 'use strict';
 
-const bowling_game = require('./bowling_game')();
+const bowling_game = require('./bowling_game');
+
+let game;
+
+const rollMany = (test_game, n, pins) => {
+  for (let i=0; i<n; i++){
+    test_game.roll(pins);
+  }
+};
 
 describe('Bowling Game', () => {
+  beforeEach(() => {
+    game = bowling_game();
+  })
+  
   it('Should create game', () => {
-    expect(bowling_game).toMatchObject({
+    expect(game).toMatchObject({
       roll: expect.any(Function),
       score: expect.any(Function)
     });
   });
   
   it('Should be able to roll', () => {
-    expect(bowling_game.roll(0)).toEqual(0);
+    expect(game.roll(0)).toEqual(0);
   });
   
   it('Should score 0 for a gutter game', () => {
-    for (let i=0; i<20; i++){
-      bowling_game.roll(0);
-    }
-    expect(bowling_game.score()).toEqual(0);
+    const n = 20;
+    const pins = 0;
+    rollMany(game, n, pins);
+    expect(game.score()).toEqual(0);
   });
   
   it('Should score 20 for all 1s', () => {
-    for (let i=0; i<20; i++){
-      bowling_game.roll(1);
-    }
-    expect(bowling_game.score()).toEqual(20);
+    const n = 20;
+    const pins = 1;
+    rollMany(game, n, pins);
+    expect(game.score()).toEqual(20);
   });
 });
