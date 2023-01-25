9d2c5878d493db053ba66bb164cb10ceaf17a125
ZT6Guy <ZT6Guy@cyber-dojo.org>
2023-01-25 06:11:23 +0000

    6 ran tests, predicted green, got green

--- files/bowling_game.test.js
+++ files/bowling_game.test.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const bowling_game = require('./bowling_game');
+const bowling_game = require('./bowling_game')();
 
 describe('Bowling Game', () => {
   it('Should create game', () => {
-    expect(bowling_game()).toMatchObject({
+    expect(bowling_game).toMatchObject({
       game: expect.any(Function)
     });
   });
